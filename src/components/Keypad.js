15c898779cba8d1bc51fadab2132d38945c4e821
Gilblasse <gilblasse@gmail.com>
2020-03-19 00:05:23 -0400

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+import React, { Component } from "react";
+
+export default class Keypad extends Component {
+  handleFocus = () => console.log("Entering password...");
+
+  render() {
+    return (
+      <div>
+        <input onKeyUp={this.handleFocus} type="password" />
+      </div>
+    );
+  }
+}
